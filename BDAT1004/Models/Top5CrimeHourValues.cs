af8531b38863be4b2603e95b5e1cb10115279991
randymay <randy.may@blaazinsoftware.com>
2018-08-14 05:56:27 -0400

    Top 5 Crimes by Hour

---
+++ BDAT1004/Models/Top5CrimeHourValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BDAT1004.Models
+{
+    public class Top5CrimeHoursValues
+    {
+        public int Year { get; set; } 
+        public int CrimeHour1 { get; set; } 
+        public int CrimeHour2 { get; set; }
+        public int CrimeHour3 { get; set; }
+        public int CrimeHour4 { get; set; }
+        public int CrimeHour5 { get; set; }
+    }
+}
---
+++ BDAT1004/Models/Top5CrimeHours.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BDAT1004.Models
+{
+    public class Top5CrimeHours
+    {
+        public List<String> columnHeaders = new List<string>();
+        public List<Top5CrimeHoursValues> columnValues = new List<Top5CrimeHoursValues>();
+    }
+}
