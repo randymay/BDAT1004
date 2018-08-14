0e0ef1f9f2ba0e508a5ecec3c125fff09c9939b0
Manu Jeevan Prakash <manu@bigdataexaminer.com>
2018-08-14 13:10:56 -0400

    commit

---
+++ BDAT1004/Models/TypesOfCrimeModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BDAT1004.Models
+{
+    public class TypesOfCrimeModel
+    {
+        public int CrimeIncident { get; set; } 
+        public int CrimeCount { get; set; } 
+    }
+}
