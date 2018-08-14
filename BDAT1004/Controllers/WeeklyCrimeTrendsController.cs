7814c94780d8790c37f9dca6476512bd39a5b718
Mishal <mishalfrancis02@gmail.com>
2018-08-14 12:56:16 -0400

    Commiting changes on MCT branches

---
+++ BDAT1004/Controllers/WeeklyCrimeTrendsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BDAT1004.Models;
+using System.Data.SqlClient;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BDAT1004.Controllers
+{
+    public class WeeklyCrimeTrendsController : Controller
+
+    {
+        #region Get data method.  
+        /// <summary>  
+        /// GET: /WeeklyCrimeTrends/GetData  
+        /// </summary>  
+        /// <returns>Return data</returns>  
+
+        public ActionResult GetData()
+        {
+            // Create resultWeekly object as a placeholder to hold results  
+            JsonResult resultWeekly = null;
+
+            try
+            {
+                // Retrieve records
+                List<WeeklyCrimeTrendsModel> data4 = this.LoadData();
+
+                // Convert data to JSON object
+                resultWeekly = new JsonResult(data4);
+            }
+            catch (Exception ex)
+            {
+                // Info  
+                Console.Write(ex);
+            }
+
+            // Return data in JSON format 
+            return resultWeekly;
+        }
+
+        #endregion
+
+        #region Load Data  
+
+        /// <summary>  
+        /// Load data method.  
+        /// </summary>  
+        /// <returns>Returns - Data</returns>  
+        private List<WeeklyCrimeTrendsModel> LoadData()
+        {
+            // Initialization.  
+            List<WeeklyCrimeTrendsModel> resultsWeekly = new List<WeeklyCrimeTrendsModel>();
+
+            try
+            {
+                // Connect to DB
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "bdat.database.windows.net";
+                builder.UserID = "adminbdat";
+                builder.Password = "Georgian123";
+                builder.InitialCatalog = "DataProgramming";
+
+
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n"); 
+
+                    // Prepare SQL
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("select crime_year, crime_day, count(case_number) as crime_count ");
+                    sb.Append("from(select year(incident_datetime) as crime_year, dayname((incident_datetime) as crime_day), case_number as case_number ");
+                    sb.Append("from dbo.Crime_Data ");
+                    sb.Append("where year(incident_datetime) >= 2013 ");
+                    sb.Append("and year(incident_datetime) <= 2017) as year_data ");
+                    sb.Append("group by crime_year,crime_day ");
+                    sb.Append("order by crime_year,crime_day");
+                    String sql = sb.ToString();
+
+                    // Open a Connection to the DB
+                    connection.Open();
+
+                    // Execute SQL
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Read Data
+                            while (reader.Read())
+                            {
+                                // Initialize Model record  
+                                WeeklyCrimeTrendsModel resultWeekly = new WeeklyCrimeTrendsModel();
+
+                                // Set values in model record
+                                resultWeekly.Day = reader.GetString(1);
+                                resultWeekly.CrimeCount = reader.GetInt32(2);
+
+                                // Add model record to collection
+                                resultsWeekly.Add(resultWeekly);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return resultsWeekly;
+        }
+
+        #endregion
+    }
+}
---
+++ BDAT1004/Models/MonthlyCrimeTrendsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDAT1004.Models
+{
+    public class MonthlyCrimeTrendsModel
+    {
+        
+        public string Month { get; set; }
+        public int CrimeCount { get; set; }
+    }
+}
